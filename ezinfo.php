6cd92ad049c19671f1da3a903fcab3bf58f2a8bd
dpobel <dpobel@7c9d12f6-6a28-4596-b04f-a285cfe9326a>
2009-06-29 16:11:01 +0000

    Added ezinfo.php file

---
+++ ezinfo.php
@@ -0,0 +1,13 @@
+<?php
+
+class ezfeedparseInfo
+{
+    static function info()
+    {
+        return array( 'Name' => '<a href="http://projects.ez.no/ezfeedparse">eZ Feed Parse</a> extension',
+                      'Version' => '0.1',
+                      'Copyright' => 'Copyright (C) 2009 Damien POBEL',
+                      'License' => 'GNU General Public License v2.0' );
+    }
+}
+?>
